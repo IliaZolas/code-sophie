b454a1e6dbc5bfd51e852ff9d4c64e68f5176e71
Ilia Zolas <ilia@thedmcsa.co.za>
2022-09-10 21:28:17 +0200

    fix/CS-19 - configuring devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  unauthenticated :user do
+    devise_scope :user do
+      get '/' => 'pages#home'
+    end
+  end
+
+  authenticated :user do
+    root 'dashboard#index'
+    
+    resources :dashboard
+  end
+
+  # resources :dashboard
+
   # root to: 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
